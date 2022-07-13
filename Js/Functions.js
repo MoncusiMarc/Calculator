0db08549945cbf7e2e081e2ebaaec8a3dfdc2f91
BCNGeekMarc <marc@bcngeek.com>
2022-07-13 14:37:19 +0200

    changes

--- Js/Functions.js
+++ Js/Functions.js
@@ -5,22 +5,50 @@ let sign;
 
 
 function clearing(){
-    console.log("we made it here");
     firstElement='0';
     secondElement='0';
     display();  
 }
 
-function insert(int){
-    if(secondElement=='0'){
-        secondElement=int;
-    }else{
-        secondElement+=int;
+function insert(element){
+    switch(true){
+        case (secondElement=='0'):
+            secondElement=element;
+            break;
+        case (secondElement.length>=10):    //TODO: function with only number counting
+            break;
+        default:
+            secondElement+=element;
+            break;
+    }
+    display();
+}
+function insertComma(){
+    switch(true){
+        case (secondElement.includes(',')):
+        case(secondElement.length>=10):     //TODO: function with only number counting
+            break;
+        case (secondElement=='0'):  
+        default:
+            secondElement+=',';
+            break;
+    }
+    display();
+}
+function polarity(){
+    switch(true){
+        case (secondElement=='0'):
+        case (secondElement=='0,'):
+            break;
+        default:
+            secondElement=-secondElement;
+            break;
     }
     display();
 }
 
 function display(){
+    console.log(secondElement);
     const display = document.getElementById("Display");
     display.innerText=secondElement;
 }

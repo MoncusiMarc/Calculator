9bbf3df4acdd0569e577a20fae8f33c4c095602f
BCNGeekMarc <marc@bcngeek.com>
2022-07-13 10:52:03 +0200

    new changes

---
+++ Js/Functions.js
@@ -0,0 +1,24 @@
+let firstElement='0';
+let secondElement='0';
+let sign;
+
+function clear(){
+    console.log("we made it here");
+    firstElement='0';
+    secondElement='0';
+    display();  
+}
+
+function insert(int){
+    if(secondElement=='0'){
+        secondElement=int;
+    }else{
+        secondElement+=int;
+    }
+    display();
+}
+
+function display(int){
+    const display = document.getElementById("Display");
+    display.innerText=secondElement;
+}

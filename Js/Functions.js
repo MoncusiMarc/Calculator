0b91530e764c5e6ef75b07a4bc1fc16ba1167f82
BCNGeekMarc <marc@bcngeek.com>
2022-07-18 03:27:55 +0200

    updates

--- Js/Functions.js
+++ Js/Functions.js
@@ -1,101 +1,150 @@
-let memoryNumber='';
-let screenNumber='';
-let sign='';
 
-const buttons = document.querySelectorAll("input");
+let memory = '';
+let screenNumber = '';
 
-function clearing(){
-    endHighlight();
-    memoryNumber='0';
-    screenNumber='0';
-    sign='';
-    display();  
+function clearing() {
+	endHighlight();
+	memory = '';
+	screenNumber = '';
+	display();
 }
 
-function insert(number){
-    switch(true){
-        case (numberCount(screenNumber)>=10):
-        case (screenNumber=="ERROR"):
+function insert(number) {
+	switch (true) {
+		case numberCount(screenNumber) >= 10:
+		case screenNumber == 'ERROR':
+			break;
+		case screenNumber == '':
+		case screenNumber == '0':
+			screenNumber = number;
+			break;
+		default:
+			screenNumber += number;
+			break;
+	}
+	display();
+}
+
+function insertComma() {
+	switch (true) {
+		case screenNumber.includes('.'):
+		case numberCount(screenNumber) >= 10:
+		case screenNumber == 'ERROR':
+			break;
+		case screenNumber == '':
+			screenNumber = '0';
+		case screenNumber == '0':
+		default:
+			screenNumber += '.';
+			break;
+	}
+	display();
+}
+
+// 12 // + // +/- // =
+// escenari linia 284?? com afecta el +/-?
+function polarity() {
+	switch (true) {
+		case screenNumber == 'ERROR':
+		case screenNumber == '':
+		case screenNumber == '0':
+		case screenNumber == '0.':
+			break;
+        case screenNumber.charAt(screenNumber.length - 1) == '.':
+            screenNumber = String(-screenNumber);
+            screenNumber += '.';
             break;
-        case (screenNumber==''):
-        case (screenNumber=='0'):
-            screenNumber=number;
-            break;
-        default:
-            screenNumber+=number;
-            break;
-    }
-    display();
+		default:
+			screenNumber = String(-screenNumber);
+			break;
+	}
+	display();
 }
 
-function insertComma(){
-    switch(true){
-        case (screenNumber.includes('.')):
-        case (numberCount(screenNumber)>=10):
-        case (screenNumber=="ERROR"):
-            break;
-        case (screenNumber==''):
-            screenNumber='0';
-        case (screenNumber=='0'):
-        default:
-            screenNumber+='.';
-            break;
-    }
-    display();
+function insertOperator(input, operator) {
+    console.log('OPERATOR');
+    console.log('MEMORY: '+memory);
+    console.log('SCREEN: '+screenNumber);
+	switch (true) {
+		case screenNumber == 'ERROR':
+			break;
+		case screenNumber != '' && memory != '':
+			equals();
+			break;
+		case screenNumber != '' && memory == '':
+			memory += screenNumber;
+			screenNumber = '';
+			break;
+		case screenNumber == '' && memory != '':
+			memory = memory.slice(0, -1);
+			break;
+		case screenNumber == '' && memory == '':
+			//TODO: que passa si apretem un operador sense que s'hagi entrat res.
+			//Utilitza el 0 del display com a primer operant o no.
+			let displayed = document.getElementById('Display').innerText.replace(',', '.');
+			memory = displayed;
+			break;
+	}
+	memory += ' ' + operator;
+	endHighlight();
+	highlight(input);
+	//display(); //TODO: deberia aparecer el numero antiguo o el 0 del proximo.
+}
+function equals() {
+	endHighlight();
+	memory += ' ' + screenNumber;
+    console.log('MEMORY: '+memory);
+    console.log('SCREEN: '+screenNumber);
+	memory = String(Math.evaluate(memory));
+
+	switch (true) {
+		case memory == 'NaN':
+		case memory == 'undefined':
+		case memory == 'Infinity':
+		case numberCount(memory) > 10:
+			screenNumber = 'ERROR';
+			display();
+			break;
+		default:
+			display(memory);
+			screenNumber = '';
+			//memory = '';
+			break;
+	}
 }
 
-function polarity(){
-    switch(true){
-        case (screenNumber=='ERROR'):
-        case (screenNumber==''):
-        case (screenNumber=='0'):
-        case (screenNumber=='0.'):
-            break;
-        default:
-            screenNumber=String(-screenNumber); //Numbers don't have replace function.
-            break;
-    }
-    display();
+function display(number = screenNumber) {
+	const display = document.getElementById('Display');
+	if (number == '') display.innerText = 0;
+	else display.innerText = number.replace('.', ',');
 }
 
-function insertOperator(input,operator){
-    endHighlight();
-    highlight(input);
-    
-    //switch(true){
-    //    case (sign==''):
-    //        memoryNumber=screenNumber;
-    //        screenNumber='';
-    //    default:
-    //        sign=operator;
-    //        break;
-    //}
-    //display();
-}
-function equals(){
-    endHighlight();
-
+function highlight(input) {
+	input.classList.add('highlight');
 }
 
-function highlight(input){
-    input.classList.add("highlight");
+function endHighlight() {
+	const buttons = document.querySelectorAll('input');
+	buttons.forEach(function (b) {
+		b.classList.remove('highlight');
+	});
 }
 
-function endHighlight(){
-            const buttons = document.querySelectorAll("input");
-    buttons.forEach(function(b){
-        b.classList.remove("highlight");
-    });
+function numberCount(numbers) {
+	return numbers.replace(/[^0-9]/g, '').length;
 }
 
-function numberCount(numbers){
-    return numbers.replace(/[^0-9]/g, '').length;
-}
+Math.evaluate = function (str) {
+	for (var i = 0; i < str.length; i++) {
+		if (isNaN(str[i]) && !['+', '-', '/', '*', '.'].includes(str[i])) {
+			return NaN;
+		}
+	}
 
-function display(){
-    console.log(memoryNumber);
-    console.log(screenNumber);
-    console.log(sign);
-    const display = document.getElementById("Display");
-    display.innerText=screenNumber.replace(".",",");
-}
+	try {
+		return eval(str);
+	} catch (e) {
+		if (e.name !== 'SyntaxError') throw e;
+		return NaN;
+	}
+};
